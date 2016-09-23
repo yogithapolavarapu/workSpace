6131f7d5f28386b308f13a6511ae8bf5e1f80f6c
yogitha <yogitha.vja@gmail.com>
2016-09-23 21:31:57 +0530

    fixed term based check

--- document_similarity/index.js
+++ document_similarity/index.js
@@ -1,8 +1,5 @@
 //remove duplicates from inputs : percentage out of bounds
 
-
-
-
 $(document).ready(function(){
   clear();
   //array to store words
@@ -11,18 +8,18 @@ $(document).ready(function(){
   //contents storage
   var content1=[],content2=[];
  var input1,input2;
- 
-//similarity checking  
-  function checkSimilar(){
+
+//similarity checking
+  function checkSimilarTerm(){
       //read the texts
     input1= $("textarea#screen1").val();
     input2=$("textarea#screen2").val();
   //to convert newlines or tabs or white spaces into another text
    input1= input1.replace(/(\r\n|\n|\r)/gm," ");
    input2= input2.replace(/(\r\n|\n|\r)/gm," ");
-    
+
        console.log(input1);
- 
+
     // check if user entered text in both areas
     if(input1 ===  "" || input2 === ""){
       alert("Please fill both the text areas");
@@ -32,11 +29,11 @@ $(document).ready(function(){
     else{               $("#level").html("Checking Similarity..");
 
   //get a variable count to raise to it for every match
-         
+
     var count=0;
     //use split to enter the statement in words
     var array=input1.split(' ');
-         
+
     //store the contents into an other
     for(var i=0;i<array.length;i++){
       content1[i]=array[i];
@@ -47,12 +44,12 @@ console.log(content1);
          console.log(content1);
          //using split to enter the statement into words
     var array=input2.split(' ');
-         
-    //store the contents into an other 
+
+    //store the contents into an other
     for(var i=0;i<array.length;i++){
       content2[i]=array[i];
     }
-         
+
          //remove duplicates from content2
          $.unique(content2);
          console.log(content2);
@@ -64,8 +61,8 @@ console.log(content1);
          else{
            var minLength= content2.length;
          }
-           
-    //compare both content1 and content 2 for similarities
+
+    //compare both content01 and content 2 for similarities
     for(var i=0;i<minLength;i++){
       for(var j=0;j<minLength;j++){
         if(content1[i]===content2[j]){
@@ -91,30 +88,29 @@ console.log(content1);
          console.log(count);
     }
   }
-  
+
   function level(count){
-    $('#percentage').html(count+"%");
     if(count>75){
-      $('#level').html('similarity level:high');
+      $('#level').html('similarity level: high');
     }
     else if(count>50){
-       $('#level').html('Similarity level:medium');
+       $('#level').html('Similarity level: medium');
     }
     else{
-        $('#level').html('similarity level :low');
+        $('#level').html('similarity level: low');
     }
   }
-  
-  $('#phrase_check').click(function(){
-   
-  checkSimilar();
-  }); 
+
+  $('#term_check').click(function(){
+
+  checkSimilarTerm();
+  });
   function clear(){
     $('#screen1').val("");
     $('#screen2').val("");
      $('#level').html("");
     $('#percentage').html("");
-    $('#words').html("");
+
   }
   $('#clear').click(function(){
     clear();
